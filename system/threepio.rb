64fb6657187511ac5ef1e6ffac753c18937df486
Jason Gilbertson <jgilbertson@customink.com>
2015-02-05 11:13:48 -0500

    Communicate over tcp Node->Ruby

--- system/threepio.rb
+++ system/threepio.rb
@@ -1,16 +1,23 @@
 require 'artoo'
+require 'socket'
 
 connection :sphero, :adaptor => :sphero, :port => '/dev/tty.Sphero-RWR-RN-SPP'
 device :sphero, :driver => :sphero
 
+socket = TCPServer.new(7570)
+
 work do
-  loop do
-    begin
-      puts "Enter a command:"
-      input = gets
-      eval(input)
-    rescue SyntaxError => se
-      puts se
+  while client = socket.accept
+    puts "Connection received from #{client.peeraddr.inspect}"
+    loop do
+      begin
+        while input = client.gets
+          eval(input.gsub('\n', ';').gsub(';;', ';'))
+        end
+      rescue SyntaxError, NoMethodError, NameError => e
+        puts e
+      end
     end
+    puts "Connection from #{client.peeraddr.inspect} terminated"
   end
 end
